2e4f88b02e24ad8857f3aa4518a5d1273b2893b2
Anmol-png <anmolkumari.0248@gmail.com>
2025-12-25 12:53:25 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+
+st.set_page_config(page_title="AI Defect Detector", page_icon="🔍")
+st.title("🔍 AI Defect Detector")
+st.write("Detects 6 defects: Crazing, Inclusion, Patches, Pitted Surface, Rolled-in Scale, Scratches")
+
+model = YOLO("/content/runs/detect/train/weights/best.pt")
+
+img = st.camera_input("📷 Take a photo")
+
+if img is not None:
+    image = Image.open(img).convert("RGB")
+    st.image(image, caption="Captured Image")
+
+    if st.button("🚀 Detect Defects"):
+        results = model(image, conf=0.3)
+        st.image(results[0].plot(), caption="Detected Defects")
